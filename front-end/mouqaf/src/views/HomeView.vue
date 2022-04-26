fca10e8513f3199bb6002cf7ce22582e1a38d3b1
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-04-26 22:58:11 +0000

    add third section pn front-end

--- front-end/mouqaf/src/views/HomeView.vue
+++ front-end/mouqaf/src/views/HomeView.vue
@@ -53,6 +53,46 @@
         </div>
       </div>
     </div>
+    <div class="third">
+      <div class="title text-center">
+        <h3>What's great about it?</h3>
+      </div>
+      <div class="third-bot">
+        <div class="brow">
+          <div class="brow-top">
+            <div class="brow-img">
+              <img src="../assets/svg/portfolio.svg" alt="">
+            </div>
+            <h4>Browse Portfolio</h4>
+          </div>
+          <div class="brow-bot">
+            <p>Find professionals you can trust by browsing their samples of previous work and reading their profile reviews.</p>
+          </div>
+        </div>
+        <div class="brow">
+          <div class="brow-top">
+            <div class="brow-img">
+              <img src="../assets/svg/bids.svg" alt="">
+            </div>
+            <h4>Fast Bids</h4>
+          </div>
+          <div class="brow-bot">
+            <p>Receive obligation free quotes from our talented freelancers fast. 85% of projects get bid on within one minute.</p>
+          </div>
+        </div>
+        <div class="brow">
+          <div class="brow-top">
+            <div class="brow-img" id="img">
+              <img src="../assets/svg/quality.svg" alt="">
+            </div>
+            <h4>Quality Work</h4>
+          </div>
+          <div class="brow-bot">
+            <p>By far the largest pool of qualified handymen is found on Mouqaf.ma</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -116,14 +156,17 @@ export default {
 .second{
   background-color: #fff;
   width: 100%;
-  height: 85vh;
+  height: 100vh;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   padding: 2rem;
 }
 .second .top h3{
   color: #3347A3;
+  font-family: 'poppins';
+  font-size: 2rem;
 }
 .bottom{
   width: 100%;
@@ -145,10 +188,10 @@ export default {
   padding: 1rem;
   box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.2);
 }
-.title h4{
-  color: #6787FE;
+.title h3{
+  color: #3347A3;
   font-family: 'poppins';
-  font-size: 1.5rem;
+  font-size: 2rem;
 }
 .img-card{
   width: 200px;
@@ -160,5 +203,52 @@ export default {
   font-size: 1rem;
   font-family: 'poppins';
   text-align: center;
+}
+
+.third{
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  /* align-items: center; */
+  justify-content: center;
+}
+.third-bot{
+  width: 100%;
+  height: auto;
+  display: flex;
+  padding: 1rem;
+  justify-content: space-around;
+}
+.brow{
+  width: 30%;
+  padding-top: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+.brow-top{
+  display: flex;
+  width: 100%;
+  height: 100px;
+  /* background-color: red; */
+  justify-content: space-around;
+  align-items: center;
+  padding-bottom: 1rem;
+}
+.brow-img{
+  width: 70px;
+}
+#img{
+  width: 50px !important;
+}
+.brow-img img{
+  width: 100%;
+}
+.brow-top h4{
+  color: #3347A3;
+  font-family: 'poppins';
+  font-size: 1.5rem;
 }
 </style>

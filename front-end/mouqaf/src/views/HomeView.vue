38a5474d9308286100fb8f3004140f20215e0e87
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-04-29 23:28:17 +0000

    add text animation

--- front-end/mouqaf/src/views/HomeView.vue
+++ front-end/mouqaf/src/views/HomeView.vue
@@ -5,8 +5,8 @@
       <div class="left">
         <p>
           Hire the best 
-          <span class="typed-text"></span> 
-          <span class="cursor">&nbsp;</span>
+          <span class="typed-text text-info"> {{typeValue}} </span> 
+          <span class="cursor" :class="{'typing': typeStatus} " >&nbsp;</span>
           <br> for any job, 
           <br> online.
         </p>
@@ -111,7 +111,57 @@ export default {
   name: 'HomeView',
   components: {
     HomeHeader
-}
+  },
+  data:() =>{
+    return{
+      typeValue:'',
+      typeStatus: false,
+      typeArray: ['Electrician','Plumber','Gardener','Painter','Handyman'],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0,
+    }
+  },
+  methods:{
+    typeText(){
+      if(this.charIndex < this.typeArray[this.typeArrayIndex].length )
+      {
+        if(!this.typeStatus)
+        this.typeStatus = true;
+        this.typeValue += this.typeArray[this.typeArrayIndex].charAt(this.charIndex);
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      }
+      else
+      {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay )
+      }
+    },
+    eraseText(){
+      if(this.charIndex > 0)
+      {
+        if(!this.typeStatus)
+        this.typeStatus = true;
+        this.typeValue = this.typeArray[this.typeArrayIndex].substring(0, this.charIndex - 1);
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed )
+      }
+      else
+      {
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+        if(this.typeArrayIndex >= this.typeArray.length)
+        this.typeArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000 );
+      }
+    }
+  },
+  created(){
+    setTimeout(this.typeText, this.newTextDelay + 200)
+  }
 }
 </script>
 
@@ -147,13 +197,25 @@ export default {
   font-family: 'poppins';
 }
 span.typed-text{
-  color: #000;
+  font-size: 2.5rem;
+  /* color: rgb(153, 12, 169); */
+  font-weight: 500;
+  font-family: 'poppins';
 }
 span.cursor{
   display: inline-block;
   margin-left: 3px;
   width: 4px;
   background-color: #fff;
+  animation: cursorBlink 1s infinite;
+}
+span.cursor.typing{
+  animation: none;
+}
+@keyframes cursorBlink{
+  49% {background-color: #fff;}
+  50% {background-color: transparent;}
+  99% {background-color: transparent;}
 }
 .bott{
   width: 100%;

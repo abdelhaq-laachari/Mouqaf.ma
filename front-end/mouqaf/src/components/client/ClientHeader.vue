b387ad7bad2904ba96fa48e0dd46564632c16285
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-05-06 15:32:29 +0000

    dropdown menu

--- front-end/mouqaf/src/components/client/ClientHeader.vue
+++ front-end/mouqaf/src/components/client/ClientHeader.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="dropdown">
-    <div class="client__img">
+  <div class="header__client container">
+    <div class="header__logo">
       <img src="../../assets/logo/mouqaf.png" alt="" />
     </div>
-    <div class="menu__drop">
-      <button
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton1"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        Client Name
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
+    <div class="dropdown">
+      <div class="client__img">
+        <img src="../../assets/img/c-3.jpg" width="150px" alt="" />
+      </div>
+      <div class="menu__drop">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Client Name
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +34,7 @@ export default {
 </script>
 
 <style scoped>
-.dropdown {
-  display: flex;
+.header__client{
+    width: 100%;
 }
 </style>

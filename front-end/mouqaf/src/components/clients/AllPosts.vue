691f2a9a7e930c8aee06d9760bff172ed4868fd0
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-05-14 16:56:43 +0100

    Creation Post

--- front-end/mouqaf/src/components/clients/AllPosts.vue
+++ front-end/mouqaf/src/components/clients/AllPosts.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <div class="all__posts">
+        <div class="post">
+            <div class="post__title">
+                <h3>Post Title</h3>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>

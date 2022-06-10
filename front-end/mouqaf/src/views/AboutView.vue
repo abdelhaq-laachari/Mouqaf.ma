7c22154acecd3e010a30f2556323bd88ed06a419
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-06-10 11:25:20 +0100

    just update

--- front-end/mouqaf/src/views/AboutView.vue
+++ front-end/mouqaf/src/views/AboutView.vue
@@ -7,17 +7,11 @@
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
-      >
-        <path
-          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-          class="shape-fill"
-        ></path>
-      </svg>
+      ></svg>
     </div>
     <div class="first">
       <div class="left">
         <img src="../assets/svg/hello.svg" alt="" />
-        <!-- <lottie-animation path="path/to/your/lottie-animation.json" /> -->
       </div>
       <div class="right">
         <div class="right__title">
@@ -40,13 +34,11 @@
 <script>
 import HomeHeader from "@/components/HomeHeader.vue";
 import HomeFooter from "../components/home/HomeFooter.vue";
-// import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue";
 export default {
   name: "AboutView",
   components: {
     HomeHeader,
     HomeFooter,
-    // LottieAnimation,
   },
 };
 </script>
